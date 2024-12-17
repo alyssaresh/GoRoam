5503f4159769184aa4624cd5bd8bd647b687b617
Alyssa Resh <aresh@Zipcoders-MacBook-Pro.local>
2024-12-17 14:28:10 -0500

    implemented spring boot

---
+++ src/main/java/com/goroam/goRoam/GoRoamApplication.java
@@ -0,0 +1,13 @@
+package com.goroam.goRoam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoRoamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoRoamApplication.class, args);
+	}
+
+}
